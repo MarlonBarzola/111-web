53cae97fbd2cf40e2692f236c0988f6c7dc315b4
MarlonBarzola <marlonbarzola@hotmail.com>
2022-07-29 11:14:31 -0500

    Listado en orden descendente de Trabajos

--- app/Http/Livewire/Jobs.php
+++ app/Http/Livewire/Jobs.php
@@ -9,7 +9,7 @@ class Jobs extends Component
 {
     public function render()
     {
-        $jobs = Job::all();
+        $jobs = Job::orderBy('id', 'desc')->get();
         return view('livewire.jobs', compact('jobs'));
     }
 }
